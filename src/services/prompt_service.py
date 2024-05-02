0862e5cac0422e373bde5dcd2d826d4b28d94960
Shakil_xBCC <shakilmrf8@gmail.com>
2024-05-02 12:52:06 +0600

    Contains :anguished:
    1.  get vector store in action
    2. formatting the prompt
    3. custom output parser
    4. process llm response

---
+++ src/services/prompt_service.py
@@ -0,0 +1,63 @@
+# src/services/prompt_service.py
+
+from langchain_community.vectorstores import Qdrant
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+import qdrant_client
+import textwrap
+
+load_dotenv()
+qdrant_api_key = os.getenv("QDRANT_API_KEY")
+qdrant_url = os.getenv("QDRANT_URL")
+
+# Get vector store in action
+def get_vector_store():
+    # Connect to the QdrantDB Cloud
+    client = qdrant_client.QdrantClient(
+        qdrant_url,
+        api_key=qdrant_api_key
+    )
+    
+    # Define Embeddings 
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    
+    # Vector store for Retrieval
+    vector_store = Qdrant(
+        client=client,
+        collection_name='rag-gemini',
+        embeddings=embeddings
+    )
+    
+    return vector_store
+
+# Format the prompt 
+def get_prompt(instruction, sys_prompt):
+    prompt_template =  sys_prompt + instruction
+    return prompt_template
+
+# Custom output parser
+def wrap_text_preserve_newlines(text, width=110):
+    # Split the input text into lines based on newline characters
+    lines = text.split('\n')
+
+    # Wrap each line individually
+    wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
+
+    # Join the wrapped lines back together using newline characters
+    wrapped_text = '\n'.join(wrapped_lines)
+
+    return wrapped_text
+
+# Return generated text and source in an output
+def process_llm_response(llm_response):
+    # Get parsed answer 
+    text = wrap_text_preserve_newlines(llm_response['result'])
+    
+    # Uncouple metadata and return it
+    sources=[]
+    for source in llm_response["source_documents"]:
+        sources.append(source.metadata['source'])
+    return text, list(set(sources))
