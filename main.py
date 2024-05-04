28b7ef1254c496232e95ffbe67b23d59679ffb86
shakil1819 <shakilmrf8@gmail.com>
2024-05-04 19:18:46 +0600

    Removed main  (uvicorn) , these were used to test locally without docker

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ async def run_processing():
 
     return {"message": "Processing complete!"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
 
