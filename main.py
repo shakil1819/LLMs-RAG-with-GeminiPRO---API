5bffb736dbd95861c99fb4fb3b565d4e723503ab
Shakil_xBCC <58840439+shakil1819@users.noreply.github.com>
2024-05-04 14:03:57 +0000

    removed redundant codes

--- main.py
+++ main.py
@@ -15,51 +15,5 @@ import os
 # Initialize FastAPI app
 app = FastAPI()
 
-# # Include routers
-# app.include_router(question_answer_router)
-
-# # Define vector store
-# vector_store = get_vector_store()
-
-# # Using Gemini-Pro 
-# llm = ChatGoogleGenerativeAI(model="gemini-pro", gemini_api_key=gemini_api_key, temperature=0.9, max_tokens=1024, convert_system_message_to_human=True)
-
-# # Load URLs for web scraping
-# URLs = ['https://gigalogy.com/',
-#         'https://gigalogy.com/personalization-platform/',
-#         'https://gigalogy.com/personalization-use-case/',
-#         'https://gigalogy.com/gpt-flow-platform/',
-#         'https://gigalogy.com/gcore-platform/',
-#         'https://gigalogy.com/smartads-platform/',
-#         'https://gigalogy.com/smartads-use-case/',
-#         'https://gigalogy.com/about-us/',
-#         'https://gigalogy.com/mission/',
-#         'https://www.gigalogy.com/team/',
-#         'https://gigalogy.com/career/',
-#         'https://gigalogy.com/developer-program/',
-#         'https://gigalogy.com/request-demo/',
-#         'https://gigalogy.com/event/',
-#         'https://gigalogy.com/press-room/',
-#         'https://gigalogy.com/developer/',
-#         'https://platform.gigalogy.com/solutions'
-# ]
-
-# # Define routes
-# @app.get("/")
-# async def run_processing():
-#     # Step 1: Setup embeddings and LLM
-#     embeddings = setup_embeddings()
-#     llm = setup_llm(gemini_api_key)
-
-#     # Step 2: Web scraping
-#     docs = await webscraper(URLs)
-
-#     # Step 3: Create QDrant collection
-#     create_qdrant_collection(docs, embeddings, collection_name='rag-gemini', qdrant_url=qdrant_url, qdrant_api_key=qdrant_api_key)
-
-#     return {"message": "Processing complete!"}
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
-
+# Include routers
+app.include_router(question_answer_router)
